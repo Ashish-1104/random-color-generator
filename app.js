45f70aad9a90d74c98017aefd8cca0093fd3a6c7
Ashish-1104 <ashishkokare5@gmail.com>
2022-12-15 21:00:01 +0530

    Added range sliders for red, green, blue color

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
+var randomNumber;
+var code,red,green,blue;
+
 var btn=document.querySelector(".btn");
 var copy=document.querySelector(".copy");
+var red_slider=document.querySelector("#red");
+var green_slider=document.querySelector("#green");
+var blue_slider=document.querySelector("#blue");
+
 copy.addEventListener("click",copyCode);
 btn.addEventListener("click",genColor);
+red_slider.addEventListener("input",function(){
+    red=red_slider.value;
+    red=Number(red);
+    setOutput(red,green,blue);
+});
+green_slider.addEventListener("input",function(){
+    green=green_slider.value;
+    green=Number(green);
+    setOutput(red,green,blue);
+});
+blue_slider.addEventListener("input",function(){
+    blue=blue_slider.value;
+    blue=Number(blue);
+    setOutput(red,green,blue);
+});
 
-var randomNumber;
-var code;
+
+
+function setOutput(red,green,blue){
+    // console.log(red,green,blue);
+    document.querySelector("label.red").innerHTML="Red: "+red;
+    document.querySelector("label.green").innerHTML="Green: "+green;
+    document.querySelector("label.blue").innerHTML="Blue: "+blue;
+    
+    red=red.toString(16);
+    green=green.toString(16);
+    blue=blue.toString(16);
+    if(red.length==1)
+        red="0"+red;
+    if(green.length==1)
+        green="0"+green;
+    if(blue.length==1)
+        blue="0"+blue;
+    code=red+green+blue;
+    document.body.style.transition="0";
+    document.body.style.backgroundColor="#"+code;
+    document.querySelector("h2").style.color="#"+code;
+    document.querySelector(".code").innerHTML="#"+code;
+    document.body.style.transition="0";
+}
 
 function genColor()
 {
     randomNumber=Math.random()*16777215;
     randomNumber=Math.floor(randomNumber);
-    code="#"+randomNumber.toString(16);
-    document.body.style.backgroundColor=code;
-    document.querySelector("h2").style.color=code;
-    document.querySelector(".code").innerHTML=code;
+    code=randomNumber.toString(16);
+    var color=code.match(/.{1,2}/g);
+    red=parseInt(color[0],16);
+    green=parseInt(color[1],16);
+    blue=parseInt(color[2],16);
+    // console.log(red,green,blue);
+    red_slider.value=red;
+    green_slider.value=green;
+    blue_slider.vaalue=blue;
+    setOutput(red,green,blue);
 }
 
 function copyCode()
 {
     navigator.clipboard.writeText(code);
-    copy.innerText="Copied";
+    copy.innerHTML="Copied ";
     setTimeout(function(){
-        copy.innerText="Copy Code";
+        copy.innerHTML="Copy Code";
     },2000);
 
 
